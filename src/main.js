b97cfbd8215ea9859b1cd035218a0df3ace1e959
kzk0829 <kzk0829kop@gmail.com>
2020-05-24 22:32:57 +0900

    最初のチャット表示まで

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import App from './plugins/firebase'
+// import Vue from 'vue'
+// import App from './App.vue'
 import vuetify from './plugins/vuetify';
+
+import Vue from 'vue'
+import '@babel/polyfill'
+// import './plugins/vuetify'
+import './plugins/firebase'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
